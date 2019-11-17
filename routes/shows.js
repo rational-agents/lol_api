a21d8d78a6dfeab9604bd4adae0f1c4cdc7c59d3
tigerturnedlion <jkelly.admin@gmail.com>
2019-11-17 16:36:26 -0500

    support for querying shows by comedian added. Duh

--- routes/shows.js
+++ routes/shows.js
@@ -2,19 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 const { Shows } = require('../src/database');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 /* GET list of shows based on venue */
 router.get('/', async function(req, res, next) {
 
   try{
+
     let venue = req.query.venue;
+    let comedian = req.query.comedian;
+    let data;
 
-    const data = await Shows.findAll({
-      attributes:['document'],
-      where:{ venue:venue}
-    });
-    res.send(data);
+    // query for both a venue and comedian
+    if( req.query.venue && req.query.comedian){
+        data = await Shows.findAll({
+        attributes:['document'],
+        where:{ [Op.and]:[ { venue:venue },{ document:{ [Op.contains]:{ lineup:[comedian] }}} ]}
+      });
+      res.send(data);
+    }
 
+    // query with just a venue
+    else if( req.query.venue && !req.query.comedian ){
+        data = await Shows.findAll({
+        attributes:['document'],
+        where:{ venue:venue}
+      });
+      res.send(data);
+    }
+
+    // query with just a comedian
+    else if( !req.query.venue && req.query.comedian ){
+        data = await Shows.findAll({
+        attributes:['document'],
+        where:{ document:{ [Op.contains]:{ lineup:[comedian] }}}
+      });
+      res.send(data);
+    }
   }
   catch(error){
     res.json({ error: error.message });
--- src/database.js
+++ src/database.js
@@ -6,49 +6,38 @@ const { DB_PASSWORD } = process.env;
 const { DB_HOST } = process.env;
 const { DB_DIALECT } = process.env;
 
-/*
-const database = new Sequelize({
-	host: 'localhost',
-    database: 'lol',
-	dialect: 'postgres',
-    operatorsAliases: Sequelize.Op
-  });
-*/
-
 const database = new Sequelize( DB,DB_USER,DB_PASSWORD,{
-	host: DB_HOST,
-	dialect: DB_DIALECT,
+     host: DB_HOST,
+     dialect: DB_DIALECT,
     operatorsAliases: Sequelize.Op
   });
 
 const Clubs = database.define('clubs',{
-	id:{type: Sequelize.INTEGER,primaryKey: true,autoIncrement: true}, //serial PRIMARY KEY,
-	venue:{type:Sequelize.STRING, allowNull:false}, //varchar NOT NULL,
-	address:{type:Sequelize.STRING}, //varchar,
-	owner:{type:Sequelize.STRING}, //varchar,
-	website:{type:Sequelize.STRING}, //varchar,
-	document:{type: Sequelize.JSONB, allowNull: true}
+     id:{type: Sequelize.INTEGER,primaryKey: true,autoIncrement: true}, //serial PRIMARY KEY,
+     venue:{type:Sequelize.STRING, allowNull:false}, //varchar NOT NULL,
+     address:{type:Sequelize.STRING}, //varchar,
+     owner:{type:Sequelize.STRING}, //varchar,
+     website:{type:Sequelize.STRING}, //varchar,
+     document:{type: Sequelize.JSONB, allowNull: true}
 });
 
 const Comedians = database.define('comedians',{
-  id:{type: Sequelize.INTEGER,primaryKey: true,autoIncrement: true}, // serial PRIMARY KEY,
-	first_name:{type:Sequelize.STRING, allowNull:false}, // varchar NOT NULL,
-	last_name:{type:Sequelize.STRING, allowNull:false}, // varchar,
-	stage_name:{type:Sequelize.STRING}, // varchar,
-	location:{type:Sequelize.STRING}, // varchar,
-	age:{type:Sequelize.STRING}, // varchar,
-	document:{type: Sequelize.JSONB, allowNull: true}
-
+     id:{type: Sequelize.INTEGER,primaryKey: true,autoIncrement: true}, // serial PRIMARY KEY,
+     first_name:{type:Sequelize.STRING, allowNull:false}, // varchar NOT NULL,
+     last_name:{type:Sequelize.STRING, allowNull:false}, // varchar,
+     stage_name:{type:Sequelize.STRING}, // varchar,
+     location:{type:Sequelize.STRING}, // varchar,
+     age:{type:Sequelize.STRING}, // varchar,
+     document:{type: Sequelize.JSONB, allowNull: true}
 });
 
 const Shows = database.define('shows',{
-  id:{type: Sequelize.INTEGER,primaryKey: true,autoIncrement: true}, //serial PRIMARY KEY,
-  venue:{type:Sequelize.STRING, allowNull:false}, //varchar NOT NULL,
-	headline:{type:Sequelize.STRING, allowNull:false}, //varchar NOT NULL,
-	show_date:{type:Sequelize.DATEONLY}, //date NOT NULL,
-	show_time:{type:Sequelize.TIME}, //time NOT NULL
-	document:{type: Sequelize.JSONB, allowNull: true}
-
+     id:{type: Sequelize.INTEGER,primaryKey: true,autoIncrement: true}, //serial PRIMARY KEY,
+     venue:{type:Sequelize.STRING, allowNull:false}, //varchar NOT NULL,
+     headline:{type:Sequelize.STRING, allowNull:false}, //varchar NOT NULL,
+     show_date:{type:Sequelize.DATEONLY}, //date NOT NULL,
+     show_time:{type:Sequelize.TIME}, //time NOT NULL
+     document:{type:Sequelize.JSONB, allowNull: true}
 });
 
 module.exports = {
