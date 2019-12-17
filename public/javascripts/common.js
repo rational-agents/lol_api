37b34ff020ceece5ec1d120826511fe19c436c45
tigerturnedlion <jkelly.admin@gmail.com>
2019-12-17 10:34:51 -0500

    improved validation that string bday is a date before calculating age

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,9 +1,13 @@
+const moment = require('moment');
 
 function calculate_age( birthday ){ // birthday is a string in format: "YYYY-MM-DD"
-    let bday = new Date(birthday);
-    let ageDifMs = Date.now() - bday.getTime();
-    let ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970)
+
+    if(moment(birthday).isValid()){
+        let bday = moment(birthday);
+        let ageDifMs = Date.now() - bday.valueOf();
+        let ageDate = new Date(ageDifMs); // miliseconds from epoch
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
 };
 
 module.exports = {
--- routes/comedians.js
+++ routes/comedians.js
@@ -15,11 +15,14 @@ router.get('/', async function(req, res, next) {
       where:{ location:location }
     });
 
+    //Loop through each comedian returned and calculate their age based on dob
     data.forEach(element => {
-      let dob = element.document.dob;
-      let age = common.calculate_age(dob);
+      if(element.document.dob){
+        let dob = element.document.dob;
+        let age = common.calculate_age(dob);
 
-      element.document["age"] = age;
+        element.document["age"] = age;
+      }
     });
     res.send(data);
 
